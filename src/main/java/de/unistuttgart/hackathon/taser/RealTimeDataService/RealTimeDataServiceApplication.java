7c4d332de1fc49a354019bb2840e80872368f234
LeviUniAcc <otterbach.levi@gmail.com>
2022-05-13 23:01:49 +0200

    init

---
+++ src/main/java/de/unistuttgart/hackathon/taser/RealTimeDataService/RealTimeDataServiceApplication.java
@@ -0,0 +1,13 @@
+package de.unistuttgart.hackathon.taser.RealTimeDataService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealTimeDataServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RealTimeDataServiceApplication.class, args);
+	}
+
+}
